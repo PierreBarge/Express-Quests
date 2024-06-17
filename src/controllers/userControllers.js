b303dd12a794201049970acdb312822c041a08b8
PierreBarge <pierrebarge@hotmail.fr>
2024-06-17 14:34:56 +0200

    Commit for quest etc...

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,34 @@
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUserById = (req, res) => {
+  database
+    .query("select * from users where id = ?", [parseInt(req.params.id)])
+    .then(([user]) => {
+      if (user[0] != null) {
+        res.status(200).json(user[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
